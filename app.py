4ea7cffca98f1d0fe3d7a3c43f314e0ec497ca54
Nicole <nicole.pirca@epn.edu.ec>
2021-06-04 12:24:16 -0500

    requesty

--- app.py
+++ app.py
@@ -12,12 +12,14 @@ tag_regex_fecha = '([01]?[0-9]|2[0-3]):[0-5][0-9](:[0-5][0-9])?|GMT'
 
 @app.route('/postFacebook') 
 def users_Post():
-    group = request.args.get('group')
-    posts = get_posts(group, pages=10)
-    for post in posts:
-        fecha = re.sub(tag_regex_fecha, '', str(post['time']))
-        fechaG = re.sub('-', '/', fecha)
-        users_locs = [{
+    try:
+#caracolradio, SaludEcuador, bbcnews, SaludEcuador, MinisterioDeGobiernoEcuador,lahoraecuador, elcomerciocom 
+        group = request.args.get('group')
+        posts = get_posts(group, pages=10)
+        for post in posts:
+            fecha = re.sub(tag_regex_fecha, '', str(post['time']))
+            fechaG = re.sub('-', '/', fecha)
+            users_locs = [{
                 'descripcion': post['text'],
                 'fecha': re.sub(r'(\d+)/(\d+)/(\d+)', r'\3/\2/\1', fechaG),
                 'calificacion': '',
@@ -25,9 +27,10 @@ def users_Post():
                 'social':'Facebook',
                 'noticia': post['text']
             } for post in  posts ]
-        lista = users_locs
-    return {'post': lista} 
-   
+            lista = users_locs
+        return {'post': lista} 
+    except:
+        return 'No es un grupo publico de Facebook'
 
 @app.route('/') 
 def inicio():
