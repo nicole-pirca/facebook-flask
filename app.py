455c0b39c74d8d31cae4ffc3670f298d8ceba3de
Nicole <nicole.pirca@epn.edu.ec>
2021-06-18 11:31:15 -0500

    dev1

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 from flask_cors import CORS
 from facebook_scraper import get_posts
 import re
-
+import time
 app = Flask(__name__)
 CORS(app)
 
@@ -14,8 +14,23 @@ tag_regex_fecha = '([01]?[0-9]|2[0-3]):[0-5][0-9](:[0-5][0-9])?|GMT'
 def users_Post():
     try:
 #caracolradio, SaludEcuador, bbcnews, SaludEcuador, MinisterioDeGobiernoEcuador,lahoraecuador, elcomerciocom 
-        
-        return 'post'
+        group = request.args.get('group')
+        time.sleep(2.4)
+        posts = get_posts(group, pages=10)
+        time.sleep(2.4)
+        for post in posts:
+            fecha = re.sub(tag_regex_fecha, '', str(post['time']))
+            fechaG = re.sub('-', '/', fecha)
+            users_locs = [{
+                'descripcion': post['text'],
+                'fecha': re.sub(r'(\d+)/(\d+)/(\d+)', r'\3/\2/\1', fechaG),
+                'calificacion': '',
+                'link':post['post_url'],
+                'social':'Facebook',
+                'noticia': post['text']
+            } for post in  posts ]
+            lista = users_locs
+        return {'post': lista} 
     except:
         return 'No es un grupo publico de Facebook'
 
